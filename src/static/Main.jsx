77320c418062a0ddf1fc8b3569b73c1dc46f055c
omotosodavid <davidomotoso45+github@gmail.com>
2024-10-08 00:11:21 +0100

    adjusted padding

--- src/static/Main.jsx
+++ src/static/Main.jsx
@@ -4,7 +4,7 @@ import { CustomProvider } from "../utils/useCustomContext";
 
 const Main = () => {
   return (
-    <main className="xl:w-[75em] xl:px-0 px-4 mx-auto">
+    <main className="xl:w-[75em] xl:px-0 sm:px-4 px-2 mx-auto">
       <CustomProvider>
         <Pages />
       </CustomProvider>
